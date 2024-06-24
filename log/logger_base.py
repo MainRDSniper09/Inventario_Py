b453622722e2e647d27df21bb79df86bd1e3a07f
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-24 15:45:52 -0500

    Se crea la clase logger_base.py, para registrar los datos en un archivo.log

---
+++ log/logger_base.py
@@ -0,0 +1,12 @@
+# Configuracion handler
+import logging as log
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('registro_datos.log'),
+                    log.StreamHandler()
+                ])
+if __name__ == '__main__':
+    log.debug('Prueba debug')
